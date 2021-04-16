e2d1424a87ab9b6e7ad901e3031c9611b945842e
Dale Wray <dalewray@gmail.com>
2021-04-16 17:37:01 -0400

    initial working

---
+++ README.md
@@ -0,0 +1,17 @@
+# mango-to-openurl
+
+Options are available in config.json or may be passed via the command line.
+
+`yarn prod` will run with options in config.json
+`yarn test` will run only the first 10 records
+`yarn log` will run yarn prod and save standard output to log.txt
+
+pass arguments example:
+`yarn prod testTrueOrFalse rateLimitInMicroseconds OldStemURL NewStemURL Primo_view_code InputFile.csv OutputFile.csv`
+
+If an asset does not have a url matching oldStemUrl the record ID will be output and skipped with a message
+
+If an assert does not have a url, an attempt to create on based on the record title will be generated and added to a new column 'SuggestedUrl'
+
+If an asset has a url, the existing url will be replaced and a message with the new url will be output.
+
---
+++ stream.js
@@ -0,0 +1,134 @@
+
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+
+const cheerio = require('cheerio');
+const csv = require('csv');
+const http = require('follow-redirects').http;
+const config = require('./config.json');
+const { parse } = require('path');
+
+const 
+  test = process.argv[8] || config.test || false,
+  rateLimit = process.argv[7] || config.rateLimit,
+  oldStemUrl = process.argv[2] || config.oldStemUrl,
+  newStemUrl = process.argv[3] || config.newStemUrl,
+  openUrlVid = process.argv[4] || config.openUrlVid,
+  inputFileName = process.argv[5] || config.inputFileName,
+  outputFileName = process.argv[6] || config.outputFileName, 
+
+const readFile = path.join(__dirname, inputFileName);
+const writeFile = path.join(__dirname, outputFileName);
+
+
+const fileContent = fs.createReadStream(readFile);
+
+const writeStream = fs.createWriteStream(writeFile);
+let parseParams = {columns: true, bom: true};
+if (test) parseParams.to_line = 10;
+const parser = csv.parse(parseParams);
+
+const transformer = csv.transform(async (row, cb) => {
+  // limit rate so we don't get rejects or banned
+  parser.pause();
+  setTimeout(() => {parser.resume()}, rateLimit);
+
+  let mangoUrl = row.URL;
+  let mangoTitle = row.Name;
+  let isbn = '';
+  
+  if ((mangoUrl && mangoUrl.includes(oldStemUrl))) {
+    data = await makeRequest(mangoUrl);
+    let data2 = {};
+    let $ = await cheerio.load(data);
+    // mango uses improper forwarding, need to pull out the new url and retrieve
+    let forward = $("meta").attr("content").substring('0; URL'.length + 1);
+    if (forward) {
+      data2 = await makeRequest(forward);
+    } else {
+      data2 = data; // attempt to recover if mango returned a good forward
+    }
+    $ = await cheerio.load(data2);
+    isbn = $('#ISBN').text();
+  }
+
+  let openFormatUrl = openUrlFormat;
+  openFormatUrl["&rft.btitle="] = encodeURI(mangoTitle);
+  if (isbn) openFormatUrl["&rft.isbn="] = isbn;
+
+  let newOpenFormatUrl = newStemUrl;
+  
+  if (mangoUrl === '' || mangoUrl.includes(oldStemUrl)) {
+    for (const [key, value] of Object.entries(openFormatUrl)) {
+      newOpenFormatUrl = newOpenFormatUrl + key + value
+    }
+    console.log(`Changed ${row.ID} URL to: `, newOpenFormatUrl);
+  } else {
+    newOpenFormatUrl = mangoUrl;
+    console.log('Did not change URL for record ', row.ID);
+  }
+
+  if (!mangoUrl) {
+    row.SuggestedUrl = newOpenFormatUrl;
+    console.log('Attempting new open url based on title');
+  } else {
+    row.URL = newOpenFormatUrl;
+    row.SuggestedUrl = '';
+  }
+
+  cb(null, row);
+});
+
+const stringify = csv.stringify({ header: true });
+
+fileContent.pipe(parser).pipe(transformer).pipe(stringify).pipe(writeStream);
+
+const openUrlFormat = {
+  "&vid=": openUrlVid,
+  "&ctx_ver=": "Z39.88-2004",
+  "&rft.genre=": "book",
+  "&ctx_enc=": "info:ofi%2Fenc:UTF-8",
+  "&url_ver=": "Z39.88-2004",
+  "&url_ctx_fmt=": "infofi%2Ffmt:kev:mtx:ctx",
+  "&rft.isbn=": ""
+};
+
+
+const makeRequest = async function (url) {
+  return new Promise((resolve, reject) => {
+    let body = '';
+    if (url.includes('https')) {
+      const req = https.request(new URL(url), res => {
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+
+        res.on('end', () => {
+          resolve(body);
+        });
+      });
+      req.on('error', error => {
+        console.error(error);
+      })
+
+      req.end();
+    } else {
+      const req = http.request(new URL(url), res => {
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+
+        res.on('end', () => {
+          resolve(body);
+        });
+        req.on('error', error => {
+          console.error(error);
+        })
+      });
+      req.end();
+
+    }
+
+  })
+}
